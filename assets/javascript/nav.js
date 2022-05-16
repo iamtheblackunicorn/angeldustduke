e2395535138eadd6ee8b0907273bf8c07ea15197
iamtheblackunicorn <officialrealalexanderabraham@gmail.com>
2022-05-16 02:18:18 +0200

    Docs missing.

---
+++ assets/javascript/nav.js
@@ -0,0 +1,10 @@
+function openNav(){
+  var navElement = document.getElementById('nav');
+  navElement.style.width = '100vw';
+  navElement.style.height = '100vh';
+}
+function closeNav(){
+  var navElement = document.getElementById('nav');
+  navElement.style.width = '0vw';
+  navElement.style.height = '0vh';
+}
